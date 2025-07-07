006ad710b6c3dc6c1a84429a49751c7ef2587ecc
VINNSTOREE <vingg1633@gmail.com>
2025-07-07 11:43:38 +0700

    Update qris.js

--- mutasi-qris/api/mutasi/qris.js
+++ mutasi-qris/api/mutasi/qris.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../models');
-const config = require('../../config');
+const config = require('../../config.json'); // ← ini benar
 
 router.get('/', (req, res) => {
   const apikey = req.query.apikey;
