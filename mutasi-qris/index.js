b0d0086ee6e4f316324fa4bdceb6e28cd7712868
VINNSTOREE <vingg1633@gmail.com>
2025-07-07 11:27:37 +0700

    Update index.js

--- mutasi-qris/index.js
+++ mutasi-qris/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const db = require('./models');
+const config = require('./config.json');
 
+// Middleware
 app.use(express.json());
 
+// Routes
 app.use('/webhook/okeconnect', require('./webhook/okeconnect'));
 app.use('/api/mutasi/qris', require('./api/mutasi/qris'));
 
+// Tes root endpoint
+app.get('/', (req, res) => {
+  res.send('Mutasi QRIS Server Aktif.');
+});
+
+// Start server
 app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(`🚀 Server Mutasi QRIS aktif di http://localhost:${port}`);
 });
